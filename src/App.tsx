f3d22c36c82f9dea971ac13da1afcf1b8178f6c5
tdieting <theresa.dietinger@dccs.at>
2022-04-05 09:48:24 +0200

    add landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {AboutMe} from "./about-me/AboutMe";
+import {LandingPage} from "./landing-page/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path={'/aboutme'} element={<AboutMe/>}/>
+        <Route path={'/'} element={<LandingPage/>}/>
       </Routes>
     </div>
   );
