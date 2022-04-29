fed36816b3092e81eb60cd8b1aea24fa40059460
tdieting <theresa.dietinger@dccs.at>
2022-04-29 15:04:05 +0200

    Fix i18n

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,19 +1,21 @@
 import i18n from 'i18next';
-
 import translationEN from './en/translation.json';
 import translationDE from './de/translation.json';
+import { initReactI18next } from 'react-i18next';
 
 export const resources = {
   en: {
-    translationEN
+    translation: translationEN,
   },
   de: {
-    translationDE
+    translation: translationDE,
   },
 } as const;
 
-i18n.init({
-  react: {
-    useSuspense: false
-  }
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+  resources,
 });
--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -2,17 +2,30 @@ import * as React from 'react';
 import {Link, Outlet} from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import './Navbar.scss';
+import '../i18n/config';
+import { useTranslation } from 'react-i18next';
+import {useState} from "react";
 
 export function Navbar() {
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState('en');
+  const changeLanguage = () => {
+    setLanguage(language === 'en' ? 'de': 'en');
+    console.log('language switch');
+    i18n.changeLanguage(language);
+  }
+
+  const displayLang = language === 'en' ? (<span><span className={'underlined'}>DE</span>/EN</span>) : (<span>DE/<span className={'underlined'}>EN</span></span>);
+
   return(
     <>
       <div className={'navbar'}>
         <Link to={'/portfolio'} className={'homeIconWrapper'}><HomeIcon className={'homeIcon'}/></Link>
-        <Link to={'/portfolio/aboutme'} className={'link'}>About me</Link>
+        <Link to={'/portfolio/aboutme'} className={'link'}>{t('aboutMe')}</Link>
         <Link to={'/portfolio/games'} className={'link'}>Game Design</Link>
         <Link to={'/portfolio/web'} className={'link'}>Web Design</Link>
         <Link to={'/portfolio/art'} className={'link'}>Art Projects</Link>
-        <span className={'languageSwitch'}>DE/EN</span>
+        <span className={'languageSwitch'} onClick={changeLanguage}>{displayLang}</span>
       </div>
       <Outlet/>
     </>
